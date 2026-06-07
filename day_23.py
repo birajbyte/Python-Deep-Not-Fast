38e3340d022f7527ee60d70a7f5f780551090200
Biraj <birajpathak83@gmail.com>
2026-06-07 07:34:26 +0545

    Sieve of Eratosthenes done

--- day_23.py
+++ day_23.py
@@ -17,13 +17,21 @@ for word in words:
 print(dic)
 
 # 2.Generate all primes up to n using Sieve of Eratosthenes 
-n = int(input("Enter the n number to find the prime"))
+import math
+n = int(input("Enter the n number to find the prime "))
+prime = [True] * (n+1)
+prime[0] = False
+prime[1] = False
+for i in range(2,math.isqrt(n)+1):
+    for j in range(i*i,n+1,i):
+        prime[j] = False
 result = []
 for num in range(2,n+1):
-    for i in range(2,num):
-        if num % i == 0:
-            break
-    else:
+    if prime[num]:#same as if prime[num] == True
         result.append(num)
-
 print(result)
+
+
+
+
+
